f1438985f26e382334fcbcce8e19d509326216da
danilotec <danilo0123499@gmail.com>
2025-11-06 16:31:50 -0300

    config css

--- barbearia_project/settings/base.py
+++ barbearia_project/settings/base.py
@@ -88,12 +88,14 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'core.Usuario'
+AUTH_USER_MODEL = 'core.User'
